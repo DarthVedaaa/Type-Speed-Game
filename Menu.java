c36cd076554a573294309db224e2d457ae1fbcdc
DarthVedaaa <vedantnayak2@gmail.com>
2019-11-14 00:08:22 +0530

    Added a New Game Menu with a background

---
+++ Menu.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File ;
+import java.io.* ;
+
+
+public class Menu extends JFrame implements ActionListener {
+   	private Button btn1;
+	public Menu(){
+	try {
+    		final Image backgroundImage = javax.imageio.ImageIO.read(new File("new-game.jpg"));
+    		setContentPane(new JPanel(new BorderLayout()) {
+        	@Override public void paintComponent(Graphics g) {
+            	g.drawImage(backgroundImage, 0, 0, null);
+        	}
+    	});
+	} catch (IOException e) {
+    		throw new RuntimeException(e);
+	}
+      	setLayout(new FlowLayout());
+	btn1 = new Button("New Game");
+	btn1.addActionListener(this); //this refers to your current frame
+	add(btn1);
+	setTitle("Welcome to Typer-Man-Game");
+	setSize(400, 400);
+	setVisible(true);
+}
+public static void main(String[] args){
+	new Menu();
+}
+@Override
+public void actionPerformed(ActionEvent e) {   	
+	try {
+		TyperManPanel newgame = new TyperManPanel();
+		this.setVisible(false);
+		newgame.setVisible(true);
+		this.dispose();
+	
+	}
+	catch (FileNotFoundException E)
+	{
+		System.exit(0);
+	}
+}
+}
